4a14f0ffd8257b03dcb685dbc32affa5b0517ffe
Zachary Hirsch <zhirsch@umich.edu>
2018-02-09 19:38:46 -0500

    New leiningen skeleton.

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject doll-smuggler "0.1.0-SNAPSHOT"
+  :description "Atomic Object programming challenge."
+  :url "https://github.com/zhirsch/doll-smuggler"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :main ^:skip-aot doll-smuggler.core
+  :target-path "target/%s")
